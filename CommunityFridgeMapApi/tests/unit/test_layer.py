7896d11f5d82ba271e00adab35e661a1082d2b78
Jean Paul Torre <jean.paul.ruben@gmail.com>
2022-05-20 21:11:20 -0400

    feat: CFM_13 add a class structure for the get_all function. Add more unit tests to get_all function

--- CommunityFridgeMapApi/tests/unit/test_layer.py
+++ CommunityFridgeMapApi/tests/unit/test_layer.py
@@ -6,7 +6,7 @@ from dependencies.python.db import get_ddb_connection
 def test_layer_test():
 
     ret = layer_test()
-    assert ret == "layer test"
+    assert ret == "hello world"
 
 def test_get_ddb_connection():
     connection = get_ddb_connection()
