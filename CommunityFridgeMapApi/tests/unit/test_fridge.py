e34a9f54f3b2661c03d8dfebc29b1dcfd93de91e
Jean Paul Torre <jean.paul.ruben@gmail.com>
2022-08-06 09:24:08 -0400

    feat: update fridge class with new data structure from api contract

--- CommunityFridgeMapApi/tests/unit/test_fridge.py
+++ CommunityFridgeMapApi/tests/unit/test_fridge.py
@@ -1,5 +1,3 @@
-from multiprocessing import connection
-
 from dependencies.python.db import layer_test
 from dependencies.python.db import get_ddb_connection
 import unittest
@@ -28,127 +26,165 @@ class DynamoDbMockPutItem:
 
 
 class FridgeTest(unittest.TestCase):
-    def test_required_fields(self):
-        fridge = Fridge(fridge={}, db_client=None)
-        has_required_field, field = fridge.has_required_fields()
-        self.assertEqual(field, "display_name")
-        self.assertFalse(has_required_field)
-        fridge = Fridge(
-            fridge={
-                "display_name": "test fridge",
-                "address": "63 Whipple St, Brooklyn, NY 11206",
-                "lat": 23.4523,
-                "long": 14.452,
-            },
-            db_client=None,
-        )
-        self.assertTrue(fridge.has_required_fields())
-
-    def test_set_username(self):
-        fridge = Fridge(fridge={"display_name": "The Friendly Fridge"}, db_client=None)
-        fridge.set_username()
-        self.assertEqual(fridge.username, "thefriendlyfridge")
+    def test_set_id(self):
+        fridge = Fridge(fridge={"name": "The Friendly Fridge"}, db_client=None)
+        fridge.set_id()
+        self.assertEqual(fridge.id, "thefriendlyfridge")
 
     def test_set_last_edditted(self):
-        fridge = Fridge(fridge={"display_name": "The Friendly Fridge"}, db_client=None)
+        fridge = Fridge(fridge={"name": "The Friendly Fridge"}, db_client=None)
         self.assertIsNone(fridge.last_edited)
         fridge.set_last_edited()
         self.assertIsNotNone(fridge.last_edited)
 
-    def test_is_valid_display_name(self):
-        fridge = Fridge(fridge={"display_name": "The Friendly Fridge"}, db_client=None)
-        self.assertTrue(fridge.is_valid_display_name())
-        fridge = Fridge(
-            fridge={"display_name": "The Friendly Fridge #%@/"}, db_client=None
-        )
-        self.assertFalse(fridge.is_valid_display_name())
+    def test_is_valid_name(self):
+        fridge = Fridge(fridge={"name": "The Friendly Fridge"}, db_client=None)
+        self.assertTrue(fridge.is_valid_name())
+        fridge = Fridge(fridge={"name": "The Friendly Fridge #%@/"}, db_client=None)
+        self.assertFalse(fridge.is_valid_name())
 
-    def test_add_item(self):
+    def test_add_item_invalid_name(self):
         db_client = DynamoDbMockPutItem()
         fridge = Fridge(
             fridge={
-                "display_name": "test fridge",
-                "address": "63 Whipple St, Brooklyn, NY 11206",
-                "lat": 23.4523,
+                "name": "&^@(*#(&(*$",
+                "location": {"geoLat": 124242, "geoLng": 2345235},
             },
             db_client=db_client,
         )
         response = fridge.add_item()
         self.assertFalse(response.success)
-        self.assertEqual(response.message, "Missing Required Field: long")
+        self.assertEqual(
+            response.message, "Name Can Only Contain Letters, Numbers, and Spaces"
+        )
         self.assertEqual(response.status_code, 400)
+
+    def test_add_item_missing_required_field(self):
+        db_client = DynamoDbMockPutItem()
         fridge = Fridge(
             fridge={
-                "display_name": "Test Fridge",
-                "address": "63 Whipple St, Brooklyn, NY 11206",
-                "lat": 23.4523,
-                "long": 14.452,
+                "name": "test fridge",
+                "location": {"geoLat": 12124},
+            },
+            db_client=db_client,
+        )
+        response = fridge.add_item()
+        self.assertFalse(response.success)
+        self.assertEqual(response.message, "Missing Required Field: location/geoLng")
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_item_min_length_failure(self):
+        db_client = DynamoDbMockPutItem()
+        fridge = Fridge(
+            fridge={
+                "name": "it",
+                "location": {"geoLat": 12124, "geoLng": 232523},
+            },
+            db_client=db_client,
+        )
+        response = fridge.add_item()
+        self.assertFalse(response.success)
+        self.assertEqual(response.message, "id character length must be >= 3")
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_item_success(self):
+        db_client = DynamoDbMockPutItem()
+        fridge = Fridge(
+            fridge={
+                "name": "Test Fridge",
+                "location": {"geoLat": 124242, "geoLng": 2345235},
             },
             db_client=db_client,
         )
         response = fridge.add_item()
         self.assertTrue(response.success)
-        self.assertEqual(response.message, "Fridge was succesfully added")
+        self.assertEqual(response.message, "fridge was succesfully added")
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(fridge.last_edited)
 
-    def test_format_dynamodb_item(self):
+    def test_format_dynamodb_item_v2(self):
         fridge = {
-            "display_name": "test",
-            "username": "test",
-            "address": "test",
-            "instagram": "test",
-            "info": "test",
-            "url": "test",
-            "neighborhood": "test",
-            "organizer_email": "test",
+            "id": "test",
+            "name": "test",
             "tags": [],
+            "location": {},
+            "maintainer": {},
+            "notes": "test",
             "food_accepts": [],
             "food_restrictions": [],
-            "lat": "test",
-            "long": "test",
+            "photoURL": "test",
             "last_edited": 2342353,
-            "profile_image": "test",
-            "report_timestamp": "test",
-            "report_notes": "test",
-            "report_status": "test",
-            "report_image": "test",
+            "verified": True,
+            "latest_report": {},
+            "fake_field": {},
         }
-        fridge_item = Fridge(fridge=fridge, db_client=None).format_dynamodb_item()
+        fridge_item = Fridge(fridge=fridge, db_client=None).format_dynamodb_item_v2()
         expected_response = {
-            "display_name": {"S": "test"},
-            "username": {"S": "test"},
-            "address": {"S": "test"},
-            "instagram": {"S": "test"},
-            "info": {"S": "test"},
-            "url": {"S": "test"},
-            "neighborhood": {"S": "test"},
-            "organizer_email": {"S": "test"},
+            "id": {"S": "test"},
+            "name": {"S": "test"},
             "tags": {"L": []},
+            "location": {"S": "{}"},
+            "maintainer": {"S": "{}"},
+            "notes": {"S": "test"},
             "food_accepts": {"L": []},
             "food_restrictions": {"L": []},
-            "lat": {"S": "test"},
-            "long": {"S": "test"},
+            "photoURL": {"S": "test"},
             "last_edited": {"N": 2342353},
-            "profile_image": {"S": "test"},
-            "report_timestamp": {"S": "test"},
-            "report_notes": {"S": "test"},
-            "report_status": {"S": "test"},
-            "report_image": {"S": "test"},
+            "verified": {"B": True},
+            "latest_report": {"S": "{}"},
         }
         self.assertEqual(fridge_item, expected_response)
+        fridge_item = Fridge(
+            fridge={"id": ""}, db_client=None
+        ).format_dynamodb_item_v2()
+        self.assertEqual(fridge_item, {})
 
-    def test_is_valid_username(self):
-        is_valid, message = Fridge.is_valid_username(None)
-        self.assertEqual(message, "Missing Required Field: username")
+    def test_is_valid_id(self):
+        is_valid, message = Fridge.is_valid_id(None)
+        self.assertEqual(message, "Missing Required Field: id")
         self.assertFalse(is_valid)
 
-        is_valid, message = Fridge.is_valid_username("hi-there")
-        self.assertEqual(message, "Username Must Be Alphanumeric")
+        is_valid, message = Fridge.is_valid_id("hi-there")
+        self.assertEqual(message, "id Must Be Alphanumeric")
         self.assertFalse(is_valid)
 
-        is_valid, message = Fridge.is_valid_username("hi")
+        is_valid, message = Fridge.is_valid_id("hi")
+        self.assertEqual(message, "id Must Have A Character Length >= 3 and <= 32")
+        self.assertFalse(is_valid)
+
+    def test_validate_fields_required_fields(self):
+        fridge = Fridge(fridge={}, db_client=None)
+        field_validator = fridge.validate_fields()
+        self.assertFalse(field_validator.is_valid())
+        self.assertEqual(field_validator.get_message(), "Missing Required Field: id")
+
+    def test_validate_fields_min_length(self):
+        fridge = Fridge(fridge={"id": "x"}, db_client=None)
+        field_validator = fridge.validate_fields()
+        self.assertFalse(field_validator.is_valid())
         self.assertEqual(
-            message, "Username Must Have A Character Length >= 3 and <= 32"
+            field_validator.get_message(), "id character length must be >= 3"
         )
-        self.assertFalse(is_valid)
+
+    def test_validate_fields_max_length(self):
+        fridge = Fridge(fridge={"id": "x" * 33}, db_client=None)
+        field_validator = fridge.validate_fields()
+        self.assertFalse(field_validator.is_valid())
+        self.assertEqual(
+            field_validator.get_message(), "id character length must be <= 32"
+        )
+
+    def test_validate_fields_success(self):
+        fridge = Fridge(
+            fridge={
+                "id": "good",
+                "name": "good",
+                "location": {"geoLat": 232342, "geoLng": 2342342},
+            },
+            db_client=None,
+        )
+        field_validator = fridge.validate_fields()
+        self.assertTrue(field_validator.is_valid())
+        self.assertEqual(
+            field_validator.get_message(), "All Fields Were Successfully Validated"
+        )
