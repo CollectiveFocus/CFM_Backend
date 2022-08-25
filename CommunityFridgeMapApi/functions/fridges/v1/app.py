6460dd352199ba26ec98e4e0b92b23a26bfa457c
Jean Paul Torre <jean.paul.ruben@gmail.com>
2022-08-25 16:41:04 -0400

    [CFM_57] API to Get All Fridges and Filter by Tag (#17)
    * CFM_57 API to Get All Fridges and Filter by Tag

--- CommunityFridgeMapApi/functions/fridges/v1/app.py
+++ CommunityFridgeMapApi/functions/fridges/v1/app.py
@@ -22,14 +22,16 @@ class FridgeHandler:
         to the event httpMethod
         """
         httpMethod = event.get("httpMethod", None)
-        path_parameters = event["pathParameters"] or {}
-        fridge_id = path_parameters.get("fridge_id")
+        pathParameters = event["pathParameters"] or {}
+        queryStringParameters = event["queryStringParameters"] or {}
+        tag = queryStringParameters.get("tag", None)
+        fridge_id = pathParameters.get("fridge_id", None)
         db_response = None
         if httpMethod == "GET":
             if fridge_id:
                 db_response = Fridge(db_client=ddbclient).get_item(fridge_id)
             else:
-                raise ValueError(f"Query Fridges API Not Yet Developed")
+                db_response = Fridge(db_client=ddbclient).get_items(tag=tag)
         elif httpMethod == "POST":
             pass
         else:
